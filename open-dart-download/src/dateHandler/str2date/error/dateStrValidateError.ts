805f171bbdc98497506d59802a19c0bfbbe64f54
ys_park <yunseong_park@tmax.co.kr>
2024-02-16 00:19:53 +0900

    [Download/ Feat] dateStrValidateError mixin 구현

---
+++ open-dart-download/src/dateHandler/str2date/error/dateStrValidateError.ts
@@ -0,0 +1,24 @@
+import { StrDate } from "../strDate/strDate.type";
+
+const DateValidateErrorMixin = <TBase extends Constructor<StrDate>>(
+  Base: TBase
+) => {
+  return class extends Base {
+    constructor(...args: any[]) {
+      super(...args);
+      this._validateDate();
+    }
+
+    private _validateDate() {
+      if (
+        this.date.getFullYear() !== this.year ||
+        this.date.getMonth() + 1 !== this.month ||
+        this.date.getDay() !== this.day
+      ) {
+        throw Error(`${this.str}은 유효하지 않은 날짜입니다.`);
+      }
+    }
+  };
+};
+
+export default DateValidateErrorMixin;
