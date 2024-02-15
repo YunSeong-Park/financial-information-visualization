189063910b7c3734cfd65011c2f0072f7bd3955a
ys_park <yunseong_park@tmax.co.kr>
2024-02-16 00:12:01 +0900

    [Download/ Refactor] strDate interface year, month, day 추가
    유효한 date검증 로직 추가를 위해 구현

--- open-dart-download/src/dateHandler/str2date/strDate/strDate.type.ts
+++ open-dart-download/src/dateHandler/str2date/strDate/strDate.type.ts
@@ -1,6 +1,9 @@
 export interface StrDate {
   get str(): string;
   get date(): Date;
+  get year(): number;
+  get month(): number;
+  get day(): number;
 }
 
 export interface LengthCheckStrDate extends StrDate {
