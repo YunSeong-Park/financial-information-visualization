6bd7e67af15a99cc6019f2c40a4e565e9951e08a
ys_park <yunseong_park@tmax.co.kr>
2024-02-16 17:08:51 +0900

    [Download/ Fix] 유효 날짜 확인시 날짜가 아닌 요일 인덱스를 사용하는 오류 fix
    Date.getDay() => 요일 Index
    Date.getDate() => 일

--- open-dart-download/src/dateHandler/str2date/error/dateStrValidateError.ts
+++ open-dart-download/src/dateHandler/str2date/error/dateStrValidateError.ts
@@ -13,7 +13,7 @@ const DateValidateErrorMixin = <TBase extends Constructor<StrDate>>(
       if (
         this.date.getFullYear() !== this.year ||
         this.date.getMonth() + 1 !== this.month ||
-        this.date.getDay() !== this.day
+        this.date.getDate() !== this.day
       ) {
         throw Error(`${this.str}은 유효하지 않은 날짜입니다.`);
       }
