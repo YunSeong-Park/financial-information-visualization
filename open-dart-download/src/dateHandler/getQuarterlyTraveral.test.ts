bde7d9e0135413a98f4bc46fc6fa8e68f5f7ce26
ys_park <yunseong_park@tmax.co.kr>
2024-02-17 23:41:39 +0900

    [Download/ Test] getQuarterlyTraveral 테스트 작성

---
+++ open-dart-download/src/dateHandler/getQuarterlyTraveral.test.ts
@@ -0,0 +1,66 @@
+import { QrtDate } from "./date.type";
+import getQrtTraversal from "./getQuarterlyTraversal";
+
+describe("getQuarterlyTraveral", () => {
+  test("같은 년도의 QrtDate 입력", () => {
+    const start: QrtDate = { year: 2021, qrt: 1 };
+    const end: QrtDate = { year: 2021, qrt: 4 };
+
+    const qrtTraversal = getQrtTraversal(start, end);
+    const qrtDateList = Array.from(qrtTraversal);
+
+    const expected = [
+      { year: 2021, qrt: 1 },
+      { year: 2021, qrt: 2 },
+      { year: 2021, qrt: 3 },
+      { year: 2021, qrt: 4 },
+    ];
+    expect(JSON.stringify(qrtDateList)).toBe(JSON.stringify(expected));
+  });
+
+  test("년도가 넘어가는 QrtDate 입력", () => {
+    const start: QrtDate = { year: 2021, qrt: 1 };
+    const end: QrtDate = { year: 2023, qrt: 4 };
+
+    const qrtTraversal = getQrtTraversal(start, end);
+    const qrtDateList = Array.from(qrtTraversal);
+
+    const expected = [
+      { year: 2021, qrt: 1 },
+      { year: 2021, qrt: 2 },
+      { year: 2021, qrt: 3 },
+      { year: 2021, qrt: 4 },
+      { year: 2022, qrt: 1 },
+      { year: 2022, qrt: 2 },
+      { year: 2022, qrt: 3 },
+      { year: 2022, qrt: 4 },
+      { year: 2023, qrt: 1 },
+      { year: 2023, qrt: 2 },
+      { year: 2023, qrt: 3 },
+      { year: 2023, qrt: 4 },
+    ];
+    expect(JSON.stringify(qrtDateList)).toBe(JSON.stringify(expected));
+  });
+
+  test("startDate가 endDate 이후인 경우 Error 발생 - 년도", () => {
+    const start: QrtDate = { year: 2021, qrt: 1 };
+    const end: QrtDate = { year: 2020, qrt: 4 };
+
+    const act = () => {
+      getQrtTraversal(start, end);
+    };
+
+    expect(act).toThrow("start date가 end date 이후 날짜입낟.");
+  });
+
+  test("startDate가 endDate 이후인 경우 Error 발생 - 분기", () => {
+    const start: QrtDate = { year: 2021, qrt: 4 };
+    const end: QrtDate = { year: 2021, qrt: 3 };
+
+    const act = () => {
+      getQrtTraversal(start, end);
+    };
+
+    expect(act).toThrow("start date가 end date 이후 날짜입낟.");
+  });
+});
