a796ef6abd30daaf9327fd90e4b5604c044441a8
ys_park <yunseong_park@tmax.co.kr>
2024-02-18 19:44:18 +0900

    [Download/ Feat] getCompanyList 구현

---
+++ open-dart-download/src/dart-business/company/getCompanyList.ts
@@ -0,0 +1,23 @@
+import { downloadCompanyList } from "../../api";
+import { xmlZip2jsonList } from "../../dataFarser/xmlZip2data";
+import { Company } from "./company.type";
+
+const getCompanyList = async (): Promise<Company[]> => {
+  const data = await downloadCompanyList();
+  const [json] = await xmlZip2jsonList(data);
+
+  const origineList: any[] = json.result.list;
+
+  const companyList: Company[] = origineList.map((company: any) => {
+    return {
+      corp_code: company.corp_code[0],
+      corp_name: company.corp_name[0],
+      stock_code: company.stock_code[0] || undefined,
+      modify_date: company.modify_date[0],
+    };
+  });
+
+  return companyList;
+};
+
+export default getCompanyList;
