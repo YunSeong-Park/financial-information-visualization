871107091149a2c1b7894c2b43e324bad02e27ab
ys_park <yunseong_park@tmax.co.kr>
2024-02-20 23:00:30 +0900

    [Front/ Setting] sass 세팅

--- front-app/next.config.mjs
+++ front-app/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import  path from 'path';
+import { fileURLToPath } from 'url';
+
+// 현재 파일의 경로를 얻기 위해 import.meta.url을 사용
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const nextConfig = {
+  sassOptions:  [path.join(__dirname, 'styles')],
+  experimental: {
+    serverActions: true,
+  },
+};
 
 export default nextConfig;
