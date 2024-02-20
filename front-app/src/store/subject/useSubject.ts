e115719ebb8e9961ab89efc2797475573cb748f2
ys_park <yunseong_park@tmax.co.kr>
2024-02-20 23:09:52 +0900

    [Front/ Feat] useSubject 구현

---
+++ front-app/src/store/subject/useSubject.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+export const ACCOUNT_SUBJECT = [
+  "재무상태표",
+  "손익계산서",
+  "포괄손익계산서",
+  "현금흐름표",
+] as const;
+
+type AccountSubject = (typeof ACCOUNT_SUBJECT)[number];
+
+type SubjectState = {
+  subject: AccountSubject;
+};
+
+type SubjectAction = {
+  setSubject: (subject: AccountSubject) => void;
+};
+
+export const useSubjectStore = create<SubjectAction & SubjectState>((set) => {
+  return {
+    subject: ACCOUNT_SUBJECT[0],
+    setSubject: (subject) => {
+      set({ subject });
+    },
+  };
+});
