619f5f57034899a4e115852ba87095c1b60e90b4
ys_park <yunseong_park@tmax.co.kr>
2024-02-20 23:48:51 +0900

    [Front/ Feat] selectedAccountStore 구현

---
+++ front-app/src/store/selectedAccount/selectedAccountStore.ts
@@ -0,0 +1,40 @@
+import { produce } from "immer";
+import { useEffect } from "react";
+import { create } from "zustand";
+import { useSubjectStore } from "../subject/useSubject";
+import useCorpCode from "@/hooks/useCorpCode";
+import { CompanyAccount } from "@/server/action";
+
+const useSelectedAccountStore = create<{
+  accounts: CompanyAccount[];
+  setAccounts: (accounts: CompanyAccount[]) => void;
+  toggleAccount: (account: CompanyAccount) => void;
+  reset: () => void;
+}>((set, get) => ({
+  accounts: [],
+  setAccounts: (accounts: CompanyAccount[]) => set({ accounts }),
+  reset: () => set({ accounts: [] }),
+  toggleAccount: (account) => {
+    const accounts = produce(get().accounts, (draft) => {
+      const idx = draft.findIndex(
+        (item) => item.account_name === account.account_name
+      );
+      if (idx === -1) {
+        draft.push(account);
+      } else {
+        draft.splice(idx, 1);
+      }
+    });
+    set({ accounts });
+  },
+}));
+
+export const useResetSelectedAcount = () => {
+  const company = useCorpCode();
+  const subject = useSubjectStore((state) => state.subject);
+  const reset = useSelectedAccountStore((state) => state.reset);
+
+  useEffect(reset, [company, subject, reset]);
+};
+
+export default useSelectedAccountStore;
