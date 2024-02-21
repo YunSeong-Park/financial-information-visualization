cafbbb0d224e1b4bb122fe737c826a5c5c724663
ys_park <yunseong_park@tmax.co.kr>
2024-02-21 21:48:54 +0900

    [Front/ Feat] DataRangeSelect 구현

---
+++ front-app/src/business-components/dateRangeSelect/dateRangeSelect.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import MultiSelect from "@/presentation-components/multiSelect/multiSelect";
+import { usePeriod } from "@/store/date/useDataOpt";
+
+const DateRangeSelect = () => {
+  const period = usePeriod();
+
+  return (
+    <MultiSelect
+      itemList={period.period.dateList}
+      value={period.period.period}
+      valueLength={2}
+      onValueChange={(value) => {
+        if (value.length !== 2) {
+          return;
+        }
+        const lowIndex = value[0] < value[1] ? 0 : 1;
+
+        period.setPeriod([value[lowIndex], value[1 - lowIndex]]);
+      }}
+      itemLabelRenderer={(date) => date.toString()}
+      triggerLabelRenderer={(period) =>
+        period.map((date) => date.toString()).join(" ~ ")
+      }
+    />
+  );
+};
+
+export default DateRangeSelect;
