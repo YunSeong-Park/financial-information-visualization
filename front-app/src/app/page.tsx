f686adbde0cde2b4ff502d79fbc46bfdcbbed59a
ys_park <yunseong_park@tmax.co.kr>
2024-02-21 22:31:54 +0900

    [Front/ Feat] 기본 페이지 구현

--- front-app/src/app/page.tsx
+++ front-app/src/app/page.tsx
@@ -1,95 +1,25 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
+import { Heading, Flex, Text } from "@radix-ui/themes";
 
-export default function Home() {
+import style from "./page.module.scss";
+import SearchTextField from "@/business-components/searchCompany/searchTextField";
+import SearchCompanyList from "@/business-components/searchCompany/searchCompanyList";
+
+const Home = async () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className={style.root}>
+      <Flex direction="column" className={style.container}>
+        <Flex align="center" justify="center" direction="column">
+          <Heading className={style.title}>회사 재무 분석</Heading>
+          <Text className={style.subtitle}>
+            분석하고자 하는 회사를 검색하세요.
+          </Text>
+        </Flex>
+        <SearchTextField />
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <SearchCompanyList />
+      </Flex>
     </main>
   );
-}
+};
+
+export default Home;
