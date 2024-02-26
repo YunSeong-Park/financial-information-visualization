564081c57cd5ea3a0321ecf41be353a6a4f4889f
ys_park <yunseong_park@tmax.co.kr>
2024-02-26 22:39:44 +0900

    [Front/ Feat] selectedAccountStore hashURL 연동

--- front-app/src/store/selectedAccount/selectedAccountStore.ts
+++ front-app/src/store/selectedAccount/selectedAccountStore.ts
@@ -1,40 +1,57 @@
 import { produce } from "immer";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { create } from "zustand";
 import { useSubjectStore } from "../subject/useSubject";
 import useCorpCode from "@/hooks/useCorpCode";
 import { CompanyAccount } from "@/server/getCompanyAccount";
+import { createJSONStorage, persist } from "zustand/middleware";
+import hashStorage from "../hashStorage/hashStorage";
 
-const useSelectedAccountStore = create<{
+type SelectedAccountState = {
   accounts: CompanyAccount[];
   setAccounts: (accounts: CompanyAccount[]) => void;
   toggleAccount: (account: CompanyAccount) => void;
   reset: () => void;
-}>((set, get) => ({
-  accounts: [],
-  setAccounts: (accounts: CompanyAccount[]) => set({ accounts }),
-  reset: () => set({ accounts: [] }),
-  toggleAccount: (account) => {
-    const accounts = produce(get().accounts, (draft) => {
-      const idx = draft.findIndex(
-        (item) => item.account_name === account.account_name
-      );
-      if (idx === -1) {
-        draft.push({ ...account });
-      } else {
-        draft.splice(idx, 1);
-      }
-    });
-    set({ accounts });
-  },
-}));
+};
+
+const useSelectedAccountStore = create<SelectedAccountState>()(
+  persist(
+    (set, get) => ({
+      accounts: [],
+      setAccounts: (accounts: CompanyAccount[]) => set({ accounts }),
+      reset: () => set({ accounts: [] }),
+      toggleAccount: (account) => {
+        const accounts = produce(get().accounts, (draft) => {
+          const idx = draft.findIndex(
+            (item) => item.account_name === account.account_name
+          );
+          if (idx === -1) {
+            draft.push({ ...account });
+          } else {
+            draft.splice(idx, 1);
+          }
+        });
+        set({ accounts });
+      },
+    }),
+    {
+      name: "selectedAccountStore", //
+      storage: createJSONStorage(() => hashStorage),
+    }
+  )
+);
 
 export const useResetSelectedAcount = () => {
   const company = useCorpCode();
   const subject = useSubjectStore((state) => state.subject);
   const reset = useSelectedAccountStore((state) => state.reset);
+  const isInit = useRef(true);
 
-  useEffect(reset, [company, subject, reset]);
+  useEffect(() => {
+    if (!isInit.current) reset();
+
+    isInit.current = false;
+  }, [company, subject, reset]);
 };
 
 export default useSelectedAccountStore;
