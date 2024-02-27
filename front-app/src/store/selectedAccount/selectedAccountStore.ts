a5f960a83892d95eddab142e3c8e962cbce28bc7
ys_park <yunseong_park@tmax.co.kr>
2024-02-27 21:12:14 +0900

    Revert "[Front/ Feat] selectedAccountStore hashURL 연동"
    hashURL nextjs 뒤로가기 이슈로 revert
    
    This reverts commit 564081c57cd5ea3a0321ecf41be353a6a4f4889f.

--- front-app/src/store/selectedAccount/selectedAccountStore.ts
+++ front-app/src/store/selectedAccount/selectedAccountStore.ts
@@ -1,57 +1,40 @@
 import { produce } from "immer";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { create } from "zustand";
 import { useSubjectStore } from "../subject/useSubject";
 import useCorpCode from "@/hooks/useCorpCode";
 import { CompanyAccount } from "@/server/getCompanyAccount";
-import { createJSONStorage, persist } from "zustand/middleware";
-import hashStorage from "../hashStorage/hashStorage";
 
-type SelectedAccountState = {
+const useSelectedAccountStore = create<{
   accounts: CompanyAccount[];
   setAccounts: (accounts: CompanyAccount[]) => void;
   toggleAccount: (account: CompanyAccount) => void;
   reset: () => void;
-};
-
-const useSelectedAccountStore = create<SelectedAccountState>()(
-  persist(
-    (set, get) => ({
-      accounts: [],
-      setAccounts: (accounts: CompanyAccount[]) => set({ accounts }),
-      reset: () => set({ accounts: [] }),
-      toggleAccount: (account) => {
-        const accounts = produce(get().accounts, (draft) => {
-          const idx = draft.findIndex(
-            (item) => item.account_name === account.account_name
-          );
-          if (idx === -1) {
-            draft.push({ ...account });
-          } else {
-            draft.splice(idx, 1);
-          }
-        });
-        set({ accounts });
-      },
-    }),
-    {
-      name: "selectedAccountStore", //
-      storage: createJSONStorage(() => hashStorage),
-    }
-  )
-);
+}>((set, get) => ({
+  accounts: [],
+  setAccounts: (accounts: CompanyAccount[]) => set({ accounts }),
+  reset: () => set({ accounts: [] }),
+  toggleAccount: (account) => {
+    const accounts = produce(get().accounts, (draft) => {
+      const idx = draft.findIndex(
+        (item) => item.account_name === account.account_name
+      );
+      if (idx === -1) {
+        draft.push({ ...account });
+      } else {
+        draft.splice(idx, 1);
+      }
+    });
+    set({ accounts });
+  },
+}));
 
 export const useResetSelectedAcount = () => {
   const company = useCorpCode();
   const subject = useSubjectStore((state) => state.subject);
   const reset = useSelectedAccountStore((state) => state.reset);
-  const isInit = useRef(true);
 
-  useEffect(() => {
-    if (!isInit.current) reset();
-
-    isInit.current = false;
-  }, [company, subject, reset]);
+  useEffect(reset, [company, subject, reset]);
 };
 
 export default useSelectedAccountStore;
