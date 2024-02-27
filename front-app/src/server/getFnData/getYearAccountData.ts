cf65595515a03bca4c6396387daee150a3dd2953
ys_park <yunseong_park@tmax.co.kr>
2024-02-27 19:58:39 +0900

    [Front/ Fix] 4분기 데이터가 없는 경우, 해당 년 데이터를 불러오지 못하는 현상 Fix

--- front-app/src/server/getFnData/getYearAccountData.ts
+++ front-app/src/server/getFnData/getYearAccountData.ts
@@ -43,17 +43,37 @@ export const getYearAccountData = async ({
     account_value: bigint;
   }[] =
     (await prisma.$queryRaw`
-  SELECT r.year, a.account_name, a.account_value
-  FROM "Report" r
-  JOIN "Account" a ON a.report_rept_no = r.rcept_no
-  WHERE r.corp_code = ${corp_code}
-    AND r.year BETWEEN ${period[0].getFullYear()} AND ${period[1].getFullYear()}
-    AND a.account_name IN (${Prisma.join(
-      accounts.map((account) => account.account_name)
-    )})
-    AND r.qrt = ${4}
-    AND a.sj_name = ${"재무상태표"}
-  ORDER BY r.year, a.account_name;
+  SELECT
+  r.year,
+  a.account_name,
+  a.account_value
+FROM
+  "Report" r
+JOIN
+  "Account" a ON a.report_rept_no = r.rcept_no
+LEFT JOIN (
+  SELECT
+    year,
+    MAX(qrt) AS max_qrt
+  FROM
+    "Report"
+  WHERE
+    corp_code = ${corp_code}
+    AND year BETWEEN ${period[0].getFullYear()} AND ${period[1].getFullYear()}
+    AND qrt <= 4 -- 가정: qrt는 1부터 4까지 있다고 가정
+  GROUP BY
+    year
+) AS max_qrt ON r.year = max_qrt.year AND r.qrt = max_qrt.max_qrt
+WHERE
+  r.corp_code = ${corp_code}
+  AND r.year BETWEEN ${period[0].getFullYear()} AND ${period[1].getFullYear()}
+  AND a.account_name IN (${Prisma.join(
+    accounts.map((account) => account.account_name)
+  )})
+  AND a.sj_name = ${"재무상태표"}
+ORDER BY
+  r.year,
+  a.account_name;
 `) || [];
 
   const convertedData = [...accumulateData, ...stateData]?.map((item) => ({
